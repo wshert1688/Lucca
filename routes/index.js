2fc8c8347ae48e5f5e818c76f6d9b354683c3733
wshert1688 <wsh1688@163.com>
2013-11-17 23:17:30 +0800

    ws

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
+var async = require("async"),
+    redis = require("redis"),
+    client = redis.createClient();
 
-/*
- * GET home page.
- */
+exports.index = function (req, res) {
+    client.keys("K_T*", function (err, data) {
+        async.map(data, function (t, cb) {
+            client.hgetall(t, function (err, d) {
+                cb(err, d);
+            });
+        }, function (err, results) {
+            console.log(results);
+            res.render('index', { title: 'Express', d: data, item: results });
+        });
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    })
+
 };
+
+
