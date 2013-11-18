35642b6ad6a291619a583436a22f672ed8293e4e
wshert1688 <wsh1688@163.com>
2013-11-18 22:21:21 +0800

    ws

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/send', routes.send);
+app.get('/stat', routes.stat);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var async = require("async"),
-    redis = require("redis"),
-    client = redis.createClient(6379,'218.24.198.133');
+    redis = require("redis"), comb = require("comb"),
+    client = redis.createClient(6379, '218.24.198.133');
+
+var K_T_PER = "VOD_PER_";
+var K_T_STAT = "VOD_STAT_";
+var K_T_DOWNLOAD_STAT = "DOWNLOAD_S";
 
 exports.index = function (req, res) {
     client.keys("VOD_PER*", function (err, data) {
@@ -10,11 +14,34 @@ exports.index = function (req, res) {
             });
         }, function (err, results) {
             console.log(results);
-            res.render('index', { title: 'Express', d: data, item: results });
+            res.render('index', { title: 'LIAONINGTV', d: data, item: results });
         });
-
     })
-
 };
 
+exports.send = function (req, res) {
+    var url = req.query.u;
+    var target = comb.date.format(new Date(), "yyyyMMddhhmmss") + random(4);
 
+    client.publish("task", JSON.stringify({target: target, path: "/data/", url: url}));
+    res.send({target: target, url: url});
+}
+
+exports.stat = function (req, res) {
+    var id = req.query.id;
+    client.hgetall(K_T_PER + id, function (err, result) {
+        res.send({stat: result});
+    })
+
+}
+
+
+function random(n) {
+    var chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var res = "";
+    for (var i = 0; i < n; i++) {
+        var id = Math.ceil(Math.random() * 35);
+        res += chars[id];
+    }
+    return res;
+}
